e054d9dd7dd77438fac7bd71575cdd624725fdac
lordel <lorenzo.delcroix@gmail.com>
2017-01-13 12:46:38 -0600

    modified:   it/prices.php Finished inserting all the required information. Changed the tables to more responsive ones based on materialize.css

--- it/prices.php
+++ it/prices.php
@@ -16,57 +16,72 @@
 <?php $title = 'Il Palagetto - Listino'; $lang = 'it'; include '../top.php'; ?>
 <div class="section no-pad-bot" id="index-banner">
   <div class="container">
-    <br>
-    <br>
     <h1 class="header center orange-text">Listino</h1>
   </div>
 </div>
 
 <div class="container">
     <div class="section center">
-        <h1>Prezzi Camere</h1>
-        <!--TODO: switch to responsive materialize table-->
-            <table width="98%" border="1" cellspacing="2" cellpadding="5" bgcolor="#FFFFEC">
+        <h5>Prezzi Camere</h5>
+        <table bgcolor="#FFFFEE" class="responsive-table z-depth-2">
+            <thead>
               <tr>
-                <td height="50">Bassa Stagione: Ott - Apr</td>
-                <td>€30*</td>
+                  <th>Bassa Stagione: Ott - Apr</th>
+                  <th>Alta Stagione: Mag - Set</th>
               </tr>
+            </thead>
+            <tbody>
               <tr>
-                <td height="50">Alta Stagione: Mag - Set</td>
+                <td>€30*</td>
                 <td>€40*</td>
               </tr>
-            </table>
-            <h1>Prezzi Camere Per Bambini</h1>
-            <table width="98%" border="1" cellspacing="2" cellpadding="5" bgcolor="#FFFFEC">
+            </tbody>
+        </table>
+
+        <h5>Prezzi Camere Per Bambini</h5>
+        <table bgcolor="#FFFFEE" class="responsive-table z-depth-2">
+            <thead>
               <tr>
-                <td height="50">0 - 6 Anni</td>
+                  <th>0 - 6 Anni</th>
+                  <th>7 - 12 Anni</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
                 <td>€0*</td>
-                </tr>
-              <tr>
-                <td height="50">7 - 12 Anni</td>
                 <td>€25*</td>
-                </tr>
-            </table>
-            <h6>*Prezzo a persona per notte. I bambini (fino a sei anni) che dormono in camera con i genitori non pagano.</h6>
-            <h1>Prezzi Appartamento Krisar</h1>
-            <table width="98%" border="1" cellspacing="2" cellpadding="5" bgcolor="#FFFFEC">
+              </tr>
+            </tbody>
+        </table>
+
+        <h6>
+            *Prezzo a persona per notte. I bambini (fino a sei anni) che
+            dormono in camera con i genitori non pagano.
+        </h6>
+
+        <h5>Prezzi Appartamento Krisar</h5>
+        <table bgcolor="#FFFFEE" class="responsive-table z-depth-2">
+            <thead>
               <tr>
-                <td>&nbsp;</td>
+                  <th></th>
+                  <th>Ott - Apr</th>
+                  <th>Mag - Set</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
                 <td>A notte</td>
-                <td>A Settimana</td>
-              </tr>
-              <tr>
-                <td>Ott - Apr</td>
                 <td>€70**</td>
-                <td>€450**</td>
+                <td>€80**</td>
               </tr>
               <tr>
-                <td>Mag - Set</td>
-                <td>€80**</td>
+                <td>A settimana</td>
+                <td>€450**</td>
                 <td>€500**</td>
               </tr>
-            </table>
-            <h6>**Prezzo per alloggio</h6>
+            </tbody>
+        </table>
+        <h6>**Prezzo per alloggio</h6>
     </div>
 </div>
 <?php include '../bottom.php'; ?>
