a476a845014860d7a7774f7168f49b1910fe99fc
lordel <lorenzo.delcroix@gmail.com>
2017-01-18 19:15:35 -0600

    modified:   it/location.php Changed the style for the map an added some shadows. Added text for directions.

--- it/location.php
+++ it/location.php
@@ -14,6 +14,7 @@
     limitations under the License.
 -->
 <?php $title = 'Il Palagetto - Dove Siamo'; $lang = 'it'; include '../top.php'; ?>
+<!--TODO: fix icons in the page -->
 <div class="section no-pad-bot" id="index-banner">
   <div class="container">
     <h1 class="header center orange-text">Dove Siamo</h1>
@@ -22,81 +23,109 @@
 
 
 <div class="container">
-    <div class="section google-maps">
+    <div class="section google-maps z-depth-2">
         <!--iframe with map-->
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d23050.937813363576!2d11.240200292379603!3d43.765235364792254!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1babcbdd477048e4!2sIl+Palagetto!5e0!3m2!1sen!2sus!4v1407914545604" frameborder="0" style="border:0"></iframe>
     </div>
 
-    <!--   Icon Section   -->
+    <h5 class="center">Come Raggiungerci</h5>
     <div class="row">
       <div class="col s12 m4">
         <div class="icon-block">
-          <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
-          <h5 class="center">Speeds up development</h5>
-
-          <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+            <h2 class="center light-blue-text"><i class="material-icons">airplane</i></h2>
+            <h5 class="center">Dall’aeroporto Amerigo Vespuci (Peretola)</h5>
+            <p class="light">È possibile prendere l’apposita navetta
+                <a href="http://www.ataf.net/System/files/FOLDER%20VOLA%20IN%20BUS%202.pdf"
+                    target="_blank">
+                    “Vola in Bus”
+                </a>
+                che vi porterà alla stazione Santa Maria Novella. Dalla
+                <strong>stazione SMN</strong> potete prendere le seguenti linee
+                autobus:
+                <a href="http://www.ataf.net/it/orari-e-percorsi/orari-e-linee/line-36.aspx?idC=180&idO=0&Linea=36&LN=it-IT"
+                    target="_blank">
+                    36
+                </a>
+                /
+                <a href="http://www.ataf.net/it/orari-e-percorsi/orari-e-linee/line-37.aspx?idC=180&idO=0&Linea=37&LN=it-IT"
+                    target="_blank">
+                    37
+                </a>
+                /
+                <a href="http://www.ataf.net/it/orari-e-percorsi/orari-e-linee/line-11.aspx?idC=180&idO=0&Linea=11&LN=it-IT"
+                    target="_blank">
+                    11
+                </a>
+                , ed il
+                <a href="http://www.ataf.net/it/orari-e-percorsi/orari-e-linee/elenco-linee.aspx?idC=180&LN=it-IT"
+                    target="_blank">
+                    68
+                </a>
+                negli orari notturni. La fermata è quella delle "Due Strade" per
+                 tutte le linee.
+            </p>
         </div>
       </div>
 
       <div class="col s12 m4">
         <div class="icon-block">
-          <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
-          <h5 class="center">User Experience Focused</h5>
-
-          <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+            <h2 class="center light-blue-text"><i class="material-icons">airplane</i></h2>
+            <h5 class="center">Dall'aeroporto di Pisa</h5>
+            <p class="light">
+                È possibile prendere
+                <a href="http://www.terravision.eu/italiano/pisa_prezzi_orari.html"
+                    target="_blank">
+                    l’autobus
+                </a>
+                per Firenze che porta alla stazione Santa Maria Novella. Dalla
+                <strong>stazione SMN</strong> potete prendere le seguenti linee
+                autobus:
+                <a href="http://www.ataf.net/it/orari-e-percorsi/orari-e-linee/line-36.aspx?idC=180&idO=0&Linea=36&LN=it-IT"
+                    target="_blank">
+                    36
+                </a>
+                /
+                <a href="http://www.ataf.net/it/orari-e-percorsi/orari-e-linee/line-37.aspx?idC=180&idO=0&Linea=37&LN=it-IT"
+                    target="_blank">
+                    37
+                </a>
+                /
+                <a href="http://www.ataf.net/it/orari-e-percorsi/orari-e-linee/line-11.aspx?idC=180&idO=0&Linea=11&LN=it-IT"
+                    target="_blank">
+                    11
+                </a>
+                , ed il
+                <a href="http://www.ataf.net/it/orari-e-percorsi/orari-e-linee/elenco-linee.aspx?idC=180&LN=it-IT"
+                    target="_blank">
+                    68
+                </a>
+                negli orari notturni. La fermata è quella delle "Due Strade" per
+                 tutte le linee.
+            </p>
         </div>
       </div>
 
       <div class="col s12 m4">
         <div class="icon-block">
-          <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
-          <h5 class="center">Easy to work with</h5>
-
-          <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+          <h2 class="center light-blue-text"><i class="material-icons">car</i></h2>
+          <h5 class="center">In macchina</h5>
+          <p class="light">
+              Dalla <strong>superstrada FI-PI-LI</strong>, prendere l’uscita per
+              l’autostrada A1 (nelle vicinanze di Scandicci) in direzione Roma.
+              Seguire poi le seguenti istruzioni:
+              <br>
+              Dall’<strong>autostrada A1</strong>, così come dalla <strong>
+                  superstrada Firenze-Siena</strong>, imboccare l’uscita
+                  “Impruneta” e prendere alla rotatoria la 3. uscita in
+                  direzione di Firenze. Seguite la strada principale (Via
+                  Senese) per ca. 4km. Dopo una serie di benzinai sulla sinistra
+                   trovate un cimitero e subito dopo la nostra piccola stradina.
+                    Svoltare a sinistra e salire Via Poccetti per ca. 300m
+                    finché sulla vostra destra non troverete il nostro
+                    Agriturismo Il Palagetto.
+            </p>
         </div>
       </div>
     </div>
-
-  </div>
-  <br><br>
-
-  <div class="section">
-      <!--TODO: change images and caption-->
-      <div class="row">
-<h4 class="center">Immagini:</h4>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/salotto.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/antica.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/appartamento.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/bellosguardo.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/bellosguardo2.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/colazione.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/marignolle.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/poccetti.JPG">
-</div>
-</div>
-  </div>
 </div>
 <?php include '../bottom.php'; ?>
