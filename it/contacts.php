d5bb64e5b19aff6900891c3b5c3593bbf1e99fa9
lordel <lorenzo.delcroix@gmail.com>
2017-01-20 16:23:02 -0600

    new file:   img/contacts_img/manu.jpg, modified:   it/contacts.php Added image for contacts, added all the necessary text to the contacts.php file.

--- it/contacts.php
+++ it/contacts.php
@@ -16,93 +16,40 @@
 <?php $title = 'Il Palagetto - Contatti'; $lang = 'it'; include '../top.php'; ?>
 <div class="section no-pad-bot" id="index-banner">
   <div class="container">
-    <br><br>
-    <h1 class="header center orange-text">Starter Template</h1>
+    <h1 class="header center orange-text">
+        Agriturismo Il Palagetto (SSA)
+    </h1>
     <div class="row center">
-      <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+        <h5 class="header col s12 light">
+            Parliamo Italiano, Tedesco, ed Inglese!
+        </h5>
     </div>
-    <div class="row center">
-      <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a>
-    </div>
-    <br><br>
-
   </div>
 </div>
-
 
 <div class="container">
-  <div class="section">
-
-    <!--   Icon Section   -->
-    <div class="row">
-      <div class="col s12 m4">
-        <div class="icon-block">
-          <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
-          <h5 class="center">Speeds up development</h5>
-
-          <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
-        </div>
-      </div>
-
-      <div class="col s12 m4">
-        <div class="icon-block">
-          <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
-          <h5 class="center">User Experience Focused</h5>
-
-          <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
-        </div>
-      </div>
-
-      <div class="col s12 m4">
-        <div class="icon-block">
-          <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
-          <h5 class="center">Easy to work with</h5>
-
-          <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
-        </div>
-      </div>
+    <div class="section center">
+        <p>
+            Via Bernardino Poccetti 12
+            <br>
+            50124, Firenze, Italia
+            <br>
+            Telefono: 366/2657395
+            <br>
+            Fax: 055/9900021
+            <br>
+            Email:
+            <a href="mailto:info@il-palagetto.com">
+                info@il-palagetto.com
+            </a>
+        </p>
+        <img class="responsive-img circle z-depth-2"
+        src="/img/contacts_img/manu.jpg" alt="Manuela"
+        style="max-width: 300px; max-heigth: 300px">
+        <p>
+            La nostra responsabile Manuela è disponibile ad aiutarvi con
+            qualsiasi necessitá
+        </p>
     </div>
-
-  </div>
-  <br><br>
-
-  <div class="section">
-      <!--TODO: change images and caption-->
-      <div class="row">
-<h4 class="center">Immagini:</h4>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/salotto.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/antica.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/appartamento.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/bellosguardo.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/bellosguardo2.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/colazione.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/marignolle.JPG">
-</div>
-<div class="col s6 m3">
-<img class="materialboxed responsive-img z-depth-1 hoverable"
-data-caption="Facciata" src="img/index_imgs/poccetti.JPG">
-</div>
-</div>
-  </div>
 </div>
 <?php include '../bottom.php'; ?>
