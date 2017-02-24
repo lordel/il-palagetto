6fb066edb578d72c267bb6989017e45705a2980e
lordel <lorenzo.delcroix@gmail.com>
2017-02-24 09:54:16 -0600

    modified:   en/prices.php Translated page and added included in price section

--- en/prices.php
+++ en/prices.php
@@ -13,21 +13,21 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 -->
-<?php $title = 'Il Palagetto - Listino'; $lang = 'it'; include __DIR__.'/../top.php'; ?>
+<?php $title = 'Il Palagetto - Listino'; $lang = 'en'; include __DIR__.'/../top.php'; ?>
 <div class="section no-pad-bot" id="index-banner">
   <div class="container">
-    <h1 class="header center orange-text">Listino</h1>
+    <h1 class="header center orange-text">Prices</h1>
   </div>
 </div>
 
 <div class="container">
     <div class="section center">
-        <h5>Prezzi Camere</h5>
+        <h5>Bedroom Prices</h5>
         <table bgcolor="#FFFFEE" class="responsive-table z-depth-2">
             <thead>
               <tr>
-                  <th>Bassa Stagione: Ott - Apr</th>
-                  <th>Alta Stagione: Mag - Set</th>
+                  <th>Oct - Apr</th>
+                  <th>May - Sep</th>
               </tr>
             </thead>
             <tbody>
@@ -38,12 +38,12 @@
             </tbody>
         </table>
 
-        <h5>Prezzi Camere Per Bambini</h5>
+        <h5>Bedrooms prices for children</h5>
         <table bgcolor="#FFFFEE" class="responsive-table z-depth-2">
             <thead>
               <tr>
-                  <th>0 - 6 Anni</th>
-                  <th>7 - 12 Anni</th>
+                  <th>0 - 6 Years Old</th>
+                  <th>7 - 12 Years Old</th>
               </tr>
             </thead>
             <tbody>
@@ -55,33 +55,55 @@
         </table>
 
         <h6>
-            *Prezzo a persona per notte. I bambini (fino a sei anni) che
-            dormono in camera con i genitori non pagano.
+            *Price per person per night.Children under the age of 6 sleeping in
+            the bedroom with their parents will not pay.
         </h6>
 
-        <h5>Prezzi Appartamento Krisar</h5>
+        <h5>Krisar Apartment Prices</h5>
         <table bgcolor="#FFFFEE" class="responsive-table z-depth-2">
             <thead>
               <tr>
                   <th></th>
-                  <th>Ott - Apr</th>
-                  <th>Mag - Set</th>
+                  <th>Oct - Apr</th>
+                  <th>May - Sep</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>A notte</td>
+                <td>Per Night</td>
                 <td>€70**</td>
                 <td>€80**</td>
               </tr>
               <tr>
-                <td>A settimana</td>
+                <td>Per Week</td>
                 <td>€450**</td>
                 <td>€500**</td>
               </tr>
             </tbody>
         </table>
-        <h6>**Prezzo per alloggio</h6>
+        <h6>**Price is per stay.</h6>
+    </div>
+    <div class="container">
+        <div class="section center">
+            <h5>Included in the Bedrooms' Price:</h5>
+            <ul>
+                <li>Breakfast</li>
+                <li>Towels and Bed Sheets</li>
+                <li>Daily Housekeeping </li>
+                <li>WI-FI</li>
+                <li>Private Parking</li>
+                <li>Tourist/Overnight Tax</li>
+            </ul>
+            <h5>Included in the Apartment's Price:</h5>
+            <ul>
+                <li>Towels and Bed Sheets</li>
+                <li>Cleaning on Departure</li>
+                <li>WI-FI</li>
+                <li>Private Parking</li>
+                <li>Tourist/Overnight Tax</li>
+                <li>With an additional charge of €12/day it is possible to have the breakfast service.</li>
+            </ul>
+        </div>
     </div>
 </div>
 <?php include __DIR__.'/../bottom.php'; ?>
